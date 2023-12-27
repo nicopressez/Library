c0e700c0bf94f688f38a9de679d6a7e092db7392
nicopressez <154272808+nicopressez@users.noreply.github.com>
2023-12-27 16:21:47 +0100

    Added remove button, read button now changes value
    Added a remove button, when pressed the corresponding book is removed from the array and HTML section removed
    When read/not read button is pressed, the value of the object is changed and the button shows the new value.

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ function Book(title,author,pages,read)
  myLibrary.push(this);
 }
 
+Book.prototype.toggleRead = function() {this.read = !this.read;}
+Book.prototype.deleteBook = function () {
+    bookIndex = myLibrary.findIndex( (book)=> this.title == book.title )
+    myLibrary.splice(bookIndex, 1)
+}
+
 function newBlock(book) 
 {
 const bookBlock = document.createElement("div");
@@ -31,23 +37,35 @@ const bookTitle = document.createElement("h2");
 const bookAuthor = document.createElement("h6");
 const bookPages = document.createElement("p");
 const bookRead = document.createElement("button");
+const bookRemove = document.createElement("button");
 
 bookTitle.textContent = book.title;
 bookAuthor.textContent = book.author;
 bookPages.textContent = book.pages;
 book.read == true ? bookRead.textContent ="Read" : bookRead.textContent = "Not read";
+bookRemove.textContent = "Remove";
 
 bookBlock.classList.add("bookblock");
 bookTitle.classList.add("booktitle");
 bookAuthor.classList.add("bookauthor");
 bookPages.classList.add("bookpages");
 bookRead.classList.add("bookread");
+bookRemove.classList.add("bookremove")
 
 booksList.appendChild(bookBlock);
 bookBlock.appendChild(bookTitle);
 bookBlock.appendChild(bookAuthor);
 bookBlock.appendChild(bookPages);
 bookBlock.appendChild(bookRead);
+bookBlock.appendChild(bookRemove);
+
+const bookReadButtons = document.querySelectorAll('.bookread');
+bookReadButtons.forEach(button => {
+    button.addEventListener('click', changeRead);
+});
+
+const bookRemoveButton = document.querySelectorAll('.bookremove');
+bookRemoveButton.forEach(button => {button.addEventListener('click', removeButton)});
 }
 
 function addBook (form) {
@@ -60,7 +78,7 @@ if (newTitle === '' || newAuthor === '' || newPages === ''){
     alert("Please fill in all the fields");
     return;}
 
-    Book(`${newTitle}`,`${newAuthor}`,`${newPages}`, newRead);
+    new Book(`${newTitle}`,`${newAuthor}`,`${newPages}`, newRead);
     newBlock(myLibrary[myLibrary.length - 1]);
     emptyFields();
     dialog.close();}
@@ -73,4 +91,25 @@ function emptyFields()
     newReadField.checked = false;
 }
 
+// When the read or not read button is pressed, change the value to the opposite 
+function changeRead(event){
+    const clickedButton = event.target;
+    const correspondingTitle = clickedButton.parentElement.querySelector('h2');
+    const bookTitle = correspondingTitle.textContent;
+    const correspondingBook = myLibrary.find( (book) => book.title == bookTitle );
+    correspondingBook.toggleRead();
+    correspondingBook.read == true ? clickedButton.textContent ="Read" 
+    : clickedButton.textContent = "Not read";
+}
 
+// When remove button is pressed, remove corresponding book from array and delete html block
+function removeButton(event){
+ const clickedButton = event.target;
+ const correspondingTitle = clickedButton.parentElement.querySelector('h2');
+ const bookTitle = correspondingTitle.textContent;
+ const correspondingBook = myLibrary.find((book) => book.title == bookTitle );
+ correspondingBook.deleteBook();
+
+ const bookBlock = correspondingTitle.parentNode;
+ bookBlock.remove();
+}
